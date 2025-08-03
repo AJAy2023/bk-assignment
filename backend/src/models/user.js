3e2a88b3c517642f8aabcaf01d00ac74d0f496d8
Ajay <ajayjadhav0913@gmail.com>
2025-08-03 15:34:04 +0530

    Remove tracked src/.env and update .gitignore

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,6 +1,6 @@
 
 const mongoose = require("mongoose");
-
+const becypt = require("bcrypt");
 const userschema = new mongoose.Schema({
     username:{
         type:String,
@@ -21,5 +21,25 @@ const userschema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('User', userschema);
+
+
+
+// string --> salt -> hashalgo == : new string  
+
+userschema.pre("save", async function(next){
+    if(!this.isModified("password"))
+    {
+        return next();
+    }
+       await becypt.hash(this.password, 10);
+       next();
+});
+
+// compare the pass  
+userschema.methods.isPassCorrect =  async function(pass)
+{
+       return await becypt.compare(pass, this.password)
+};
+
 
 
